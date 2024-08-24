822aab5bc0197117833f997a21606336c4779371
Juan René Hernández Sánchez <jhernandezs1509@alumno.ipn.mx>
2024-08-23 21:10:52 -0600

    Nuevas Diapositivas
    Desarrollo de Componentes, Flujos y Pipes

--- Proyecto/frontend/src/app/app.component.ts
+++ Proyecto/frontend/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+@Component({ // Decorador @Component
+  selector: 'app-root', // Nombre del Componente para llamarlo en html
+  standalone: true, // Indicador Standalone
+  imports: [RouterOutlet, ExampleComponent], // Imports de Componentes Anidados
+  templateUrl: './app.component.html', // Vista html
+  styleUrl: './app.component.css' // Estilos css
 })
+
+// Declaracion de la Clase, se incluye el Comportamiento del Componente
 export class AppComponent {
   title = 'frontend';
 }
---
+++ Proyecto/frontend/src/app/example/example.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './example.component.html',
+  styleUrl: './example.component.css',
+})
+export class ExampleComponent {
+  username = 'Fulano de Tal';
+  isLoged = true;
+  users = [
+    { id: 0, name: 'Sarah' },
+    { id: 1, name: 'Amy' },
+    { id: 2, name: 'Rachel' },
+    { id: 3, name: 'Jessica' },
+    { id: 4, name: 'Poornima' },
+  ];
+}
